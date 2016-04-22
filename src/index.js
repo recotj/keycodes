7d5ca04d4242a5710daff4f7e97cae93e4b64054
reco <reco@liao.com>
2016-04-22 18:39:17 +0800

    create repo

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/** @see https://css-tricks.com/snippets/javascript/javascript-keycodes/ **/
+module.exports = {
+	BACKSPACE: 0x08,
+	TAB: 0x09,
+	ENTER: 0x0D,
+	ESCAPE: 0x1B,
+	LEFT: 0x25,
+	UP: 0x26,
+	RIGHT: 0x27,
+	DOWN: 0x28,
+	DELETE: 0x2E,
+	NUMBER_0: 0x30,
+	NUMBER_9: 0x39,
+	NUMBER_PAD_0: 0x60,
+	NUMBER_PAD_9: 0x69,
+	A: 0x41,
+	Z: 0x5A,
+	a: 0x61,
+	z: 0x7A,
+	Symbols: [
+		0x32, // space
+		0x6A, // multiply
+		0x6B, // add
+		0x6D, // subtract
+		0x6E, // decimal point
+		0x6F, // divide
+		0xBA, // semi-colon
+		0xBB, // equal sign
+		0xBC, // comma
+		0xBD, // dash
+		0xBE, // period
+		0xBF, // forward slash
+		0xC0, // grave accent
+		0xDB, // open bracket
+		0xDC, // back slash
+		0xDD, // close braket
+		0xDE  // single quote
+	]
+};
